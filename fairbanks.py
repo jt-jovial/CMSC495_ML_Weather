c304ed969f3b24d3691ffa15372d5adcc5681838
kslloyd <103386121+kslloyd@users.noreply.github.com>
2022-05-03 15:12:21 -0400

    added try/except blocks
    Try/except blocks added to catch any general exception

--- fairbanks.py
+++ fairbanks.py
@@ -33,186 +33,203 @@ factor = 0
 
 def season_predict(temp):
     """ Season selection, used for Temperature method for Menu option 1 """
-    # Assign features (X) and target (y)
-    X = df[['Average']]
-    y = df['Season']
+    try:
+        # Assign features (X) and target (y)
+        X = df[['Average']]
+        y = df['Season']
 
-    # K-Nearest Neighbors model, 72% accuracy
-    knn = KNeighborsClassifier(n_neighbors=12)
-    # Fit the data to the model, then predict using given temperature
-    knn.fit(X.values, y)
-    season = knn.predict([[temp]])
-    # Seasons: 0 == Winter, 1 == Spring, 2 == Summer, 3 == Fall
-    # remove brackets from season
-    for i in season:
-        season = i
-    return season
-    
+        # K-Nearest Neighbors model, 72% accuracy
+        knn = KNeighborsClassifier(n_neighbors=12)
+        # Fit the data to the model, then predict using given temperature
+        knn.fit(X.values, y)
+        season = knn.predict([[temp]])
+        # Seasons: 0 == Winter, 1 == Spring, 2 == Summer, 3 == Fall
+        # remove brackets from season
+        for i in season:
+            season = i
+        return season
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+
 
 # Season will come from season_predict()
 def temperature(temp, season, daylight, hours=None):
     """ Temperature method for Menu option 1 """
-    if season == 0:
-        # Isolate winter to narrow down the target month
-        df_season = df.loc[df['Season'] == 0]
-        m_range = (210, 720)  # Winter daylight hour range in minutes
-        # Range in hours for input request - 3.5-12.0
-    elif season == 1:
-        # Isolate spring to narrow down the target month
-        df_season = df.loc[df['Season'] == 1]
-        m_range = (720, 1140)  # Spring daylight hour range in minutes
-        # Range in hours for input request - 12.0-19.0
-    elif season == 2:
-        # Isolate summer to narrow down the target month
-        df_season = df.loc[df['Season'] == 2]
-        m_range = (840, 1320)  # Summer daylight hour range in minutes
-        # Range in hours for input request - 14.0-22.0
-    elif season == 3:
-        # Isolate fall to narrow down the target month
-        df_season = df.loc[df['Season'] == 3]
-        m_range = (420, 840)  # Fall daylight hour range in minutes
-        # Range in hours for input request - 7.0-14.0
+    try:
+        if season == 0:
+            # Isolate winter to narrow down the target month
+            df_season = df.loc[df['Season'] == 0]
+            m_range = (210, 720)  # Winter daylight hour range in minutes
+            # Range in hours for input request - 3.5-12.0
+        elif season == 1:
+            # Isolate spring to narrow down the target month
+            df_season = df.loc[df['Season'] == 1]
+            m_range = (720, 1140)  # Spring daylight hour range in minutes
+            # Range in hours for input request - 12.0-19.0
+        elif season == 2:
+            # Isolate summer to narrow down the target month
+            df_season = df.loc[df['Season'] == 2]
+            m_range = (840, 1320)  # Summer daylight hour range in minutes
+            # Range in hours for input request - 14.0-22.0
+        elif season == 3:
+            # Isolate fall to narrow down the target month
+            df_season = df.loc[df['Season'] == 3]
+            m_range = (420, 840)  # Fall daylight hour range in minutes
+            # Range in hours for input request - 7.0-14.0
 
-    # If the user has a preference of number of daylight hours, their hours will be converted to minutes
-    if daylight == 1: # User has preference
-        minutes = hours * 60 
-        # Input validation for range
-        if minutes < m_range[0]:
-            minutes = m_range[0]
-        elif minutes > m_range[1]:
-            minutes = m_range[1]     
-    elif daylight == 2: # User has no preference
-        minutes = random.randint(m_range[0], m_range[1]+1)
-        
-    # Assign features (x) and target (y)
-    X = df_season[['Average', 'Daylight']]
-    y = df_season['Month']
+        # If the user has a preference of number of daylight hours, their hours will be converted to minutes
+        if daylight == 1: # User has preference
+            minutes = hours * 60 
+            # Input validation for range
+            if minutes < m_range[0]:
+                minutes = m_range[0]
+            elif minutes > m_range[1]:
+                minutes = m_range[1]     
+        elif daylight == 2: # User has no preference
+            minutes = random.randint(m_range[0], m_range[1]+1)
+            
+        # Assign features (x) and target (y)
+        X = df_season[['Average', 'Daylight']]
+        y = df_season['Month']
 
-    # Decision Tree model, 84% accuracy
-    dtree = tree.DecisionTreeClassifier()
-    # Fit the data to the model, then predict using given temperature and daylight in minutes
-    dtree.fit(X.values, y)
-    month_predict = dtree.predict([[temp, minutes]])
-    # Remove brackets from month
-    for i in month_predict:
-        month_predict = i
-    return month_predict
-
+        # Decision Tree model, 84% accuracy
+        dtree = tree.DecisionTreeClassifier()
+        # Fit the data to the model, then predict using given temperature and daylight in minutes
+        dtree.fit(X.values, y)
+        month_predict = dtree.predict([[temp, minutes]])
+        # Remove brackets from month
+        for i in month_predict:
+            month_predict = i
+        return month_predict
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise    
 
 def snowfall(snow, daylight, hours=None):
     """ Snowfall method for Menu option 2 """
-    # If the user has a preference of number of daylight hours, their hours will be converted to minutes
-    if daylight == 1: # User has preference
-        # Input validation for range
-        if hours < 3.5:
-            hours = 3.5
-        elif hours > 18.5:
-            hours = 18.5
-        minutes = hours * 60 # Need to add validation in frontend; hours must be between 3.5-18.5   
-    elif daylight == 2: # User has no preference
-        minutes = random.randint(210, 1110)
+    try:
+        # If the user has a preference of number of daylight hours, their hours will be converted to minutes
+        if daylight == 1: # User has preference
+            # Input validation for range
+            if hours < 3.5:
+                hours = 3.5
+            elif hours > 18.5:
+                hours = 18.5
+            minutes = hours * 60 # Need to add validation in frontend; hours must be between 3.5-18.5   
+        elif daylight == 2: # User has no preference
+            minutes = random.randint(210, 1110)
 
-    # Assign features (X) and target (y)
-    X = df[['Snow Depth', 'Daylight']]
-    y = df['Month']
-    # Decision Tree model, 95% accuracy
-    dtree = tree.DecisionTreeClassifier()
-    # Fit the data to the model, then predict using given snowfall amount and daylight in minutes
-    dtree.fit(X.values, y)
-    month_predict = dtree.predict([[snow, minutes]])
-    # Remove brackets from month
-    for i in month_predict:
-        month_predict = i
-    return month_predict
-
+        # Assign features (X) and target (y)
+        X = df[['Snow Depth', 'Daylight']]
+        y = df['Month']
+        # Decision Tree model, 95% accuracy
+        dtree = tree.DecisionTreeClassifier()
+        # Fit the data to the model, then predict using given snowfall amount and daylight in minutes
+        dtree.fit(X.values, y)
+        month_predict = dtree.predict([[snow, minutes]])
+        # Remove brackets from month
+        for i in month_predict:
+            month_predict = i
+        return month_predict
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
 
 def daylight_hours(hours, choice=3):
     """ Daylight hours method for Menu option 3 """
-    # Input validation for range
-    if hours < 3.5:
-        hours = 3.5
-    elif hours > 22:
-        hours = 22
-    minutes = hours * 60      
-    # Once minutes are calculated, a season can be determined
-    # Winter: 210-419, Fall/Winter: 420-719, Fall/Spring: 720-840, Spring/Summer: 841-1140, Summer: 1140-1320
-    # Winter: min < 420, Summer: 1140 < min <= 1320
-    # Fall/Winter: 420 <= min < 720, Fall/Spring: 720 <= min <= 840, Spring/Summer: 840 < min <= 1140
-    if minutes < 420:
-        season = 0  # Winter automatically chosen (less than 7 hours)
-    elif 420 <= minutes < 720:  # This range falls between 2 seasons (7-12 hours, Fall/WInter)
-        if choice == 1:
-            season = 3  # Fall
-        elif choice == 2:
-            season = 0  # Winter
-        elif choice == 3:
-            season = np.random.choice([3, 0])  # Will randomly choose between 3 and 0
-    elif 720 <= minutes <= 840:  # This range falls between 2 seasons (12-14 hours, Spring/Fall)
-        if choice == 1:
-            season = 1  # Spring
-        elif choice == 2:
-            season = 3  # Fall
-        elif choice == 3:
-            season = np.random.choice([1, 3])  # Will randomly choose between 1 and 3
-    elif 840 < minutes <= 1140:  # This range falls between 2 seasons (14-19 hours, Spring/Summer)
-        if choice == 1:
-            season = 1  # Spring
-        elif choice == 2:
-            season = 2  # Summer
-        elif choice == 3:
-            season = np.random.choice([1, 2])  # Will randomly choose between 1 and 2
-    elif 1140 < minutes <= 1320:
-        season = 2  # Summer automatically chosen (19-22 hours)
-        
-    # Assign features (X) and target (y)
-    X = df[['Season', 'Daylight']]
-    y = df['Month']
-    # Decision Tree model, 95% accuracy
-    dtree = tree.DecisionTreeClassifier()
-    # Fit the data to the model, then predict using given season and daylight in minutes
-    dtree.fit(X.values, y)
-    month_predict = dtree.predict([[season, minutes]])
-    # Remove brackets from month
-    for i in month_predict:
-        month_predict = i
-    return month_predict
-
-
-def northern_lights(daylight, snowfall_pref, hours=None, snow=None):
-    """ Northern Lights method for Menu option 4 """
-    print('The best time to see the Northern Lights in Fairbanks, AK is between August and April!')
-    # Isolate dataframe to months between August and April
-    df_nl = df.loc[df['Month'].isin([8, 9, 10, 11, 12, 1, 2, 3, 4])]
-
-    # If the user has a preference of number of daylight hours, their hours will be converted to minutes
-    if daylight == 1: # User has preference
+    try:
         # Input validation for range
         if hours < 3.5:
             hours = 3.5
-        elif hours > 16.5:
-            hours = 16.5
-        minutes = hours * 60 # Need to add validation in frontend; hours must be between 3.5-16.5  
-    elif daylight == 2: # User has no preference
-        minutes = random.randint(210, 990)
+        elif hours > 22:
+            hours = 22
+        minutes = hours * 60      
+        # Once minutes are calculated, a season can be determined
+        # Winter: 210-419, Fall/Winter: 420-719, Fall/Spring: 720-840, Spring/Summer: 841-1140, Summer: 1140-1320
+        # Winter: min < 420, Summer: 1140 < min <= 1320
+        # Fall/Winter: 420 <= min < 720, Fall/Spring: 720 <= min <= 840, Spring/Summer: 840 < min <= 1140
+        if minutes < 420:
+            season = 0  # Winter automatically chosen (less than 7 hours)
+        elif 420 <= minutes < 720:  # This range falls between 2 seasons (7-12 hours, Fall/WInter)
+            if choice == 1:
+                season = 3  # Fall
+            elif choice == 2:
+                season = 0  # Winter
+            elif choice == 3:
+                season = np.random.choice([3, 0])  # Will randomly choose between 3 and 0
+        elif 720 <= minutes <= 840:  # This range falls between 2 seasons (12-14 hours, Spring/Fall)
+            if choice == 1:
+                season = 1  # Spring
+            elif choice == 2:
+                season = 3  # Fall
+            elif choice == 3:
+                season = np.random.choice([1, 3])  # Will randomly choose between 1 and 3
+        elif 840 < minutes <= 1140:  # This range falls between 2 seasons (14-19 hours, Spring/Summer)
+            if choice == 1:
+                season = 1  # Spring
+            elif choice == 2:
+                season = 2  # Summer
+            elif choice == 3:
+                season = np.random.choice([1, 2])  # Will randomly choose between 1 and 2
+        elif 1140 < minutes <= 1320:
+            season = 2  # Summer automatically chosen (19-22 hours)
+            
+        # Assign features (X) and target (y)
+        X = df[['Season', 'Daylight']]
+        y = df['Month']
+        # Decision Tree model, 95% accuracy
+        dtree = tree.DecisionTreeClassifier()
+        # Fit the data to the model, then predict using given season and daylight in minutes
+        dtree.fit(X.values, y)
+        month_predict = dtree.predict([[season, minutes]])
+        # Remove brackets from month
+        for i in month_predict:
+            month_predict = i
+        return month_predict
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
 
-    # if snowfall_pref == 1, the user entered a value for snow that will be accepted
-    # if snowfall_pref == 2, a random snow depth will be generated between 0-24 inches
-    if snowfall_pref == 2:
-        snow = random.randint(0, 24)
+def northern_lights(daylight, snowfall_pref, hours=None, snow=None):
+    """ Northern Lights method for Menu option 4 """
+    try:
+        print('The best time to see the Northern Lights in Fairbanks, AK is between August and April!')
+        # Isolate dataframe to months between August and April
+        df_nl = df.loc[df['Month'].isin([8, 9, 10, 11, 12, 1, 2, 3, 4])]
 
-    # Assign features (X) and target (y)
-    X = df_nl[['Snow Depth', 'Daylight']]
-    y = df_nl['Month']
-    # Decision Tree model, 95% accuracy
-    dtree = tree.DecisionTreeClassifier()
-    # Fit the data to the model, then predict using given season and daylight in minutes
-    dtree.fit(X.values, y)
-    month_predict = dtree.predict([[snow, minutes]])
-    # Remove brackets from month
-    for i in month_predict:
-        month_predict = i
-    return month_predict
+        # If the user has a preference of number of daylight hours, their hours will be converted to minutes
+        if daylight == 1: # User has preference
+            # Input validation for range
+            if hours < 3.5:
+                hours = 3.5
+            elif hours > 16.5:
+                hours = 16.5
+            minutes = hours * 60 # Need to add validation in frontend; hours must be between 3.5-16.5  
+        elif daylight == 2: # User has no preference
+            minutes = random.randint(210, 990)
 
+        # if snowfall_pref == 1, the user entered a value for snow that will be accepted
+        # if snowfall_pref == 2, a random snow depth will be generated between 0-24 inches
+        if snowfall_pref == 2:
+            snow = random.randint(0, 24)
+
+        # Assign features (X) and target (y)
+        X = df_nl[['Snow Depth', 'Daylight']]
+        y = df_nl['Month']
+        # Decision Tree model, 95% accuracy
+        dtree = tree.DecisionTreeClassifier()
+        # Fit the data to the model, then predict using given season and daylight in minutes
+        dtree.fit(X.values, y)
+        month_predict = dtree.predict([[snow, minutes]])
+        # Remove brackets from month
+        for i in month_predict:
+            month_predict = i
+        return month_predict
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+ 
 
 def test_program(factor):
     """ While loop for backend testing of Main Menu """

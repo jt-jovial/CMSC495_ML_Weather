f2375b7550dab60cc586d61b4610a4295ee3ccff
kslloyd <103386121+kslloyd@users.noreply.github.com>
2022-05-04 08:22:14 -0400

    additional line breaks for readability

--- month_content.py
+++ month_content.py
@@ -9,123 +9,123 @@ Creates an importable dictionary which is fairly easy to edit and insert into th
 """
 
 January = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://www.pawsforadventure.com/dog-mushing-day-tours/" target="_blank">Dog Sledding</a><br>
 <a href="https://www.travelalaska.com/Things-To-Do/Northern-Lights" target="_blank">Northern Lights info and tours</a><br>
-<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br>
-Lodging:<br>
+<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br><br>
+Lodging:<br><br>
 <a href="https://www.borealisbasecamp.net/homepage" target="_blank">Igloo experience with northern lights</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 February = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://www.irondog.org/events/fairbanks" target="_blank">Iron Dog snowmobile race</a><br>
 <a href="https://www.icealaska.org/" target="_blank">Ice Art Around Town</a><br>
 <a href="https://www.travelalaska.com/Things-To-Do/Northern-Lights" target="_blank">Northern Lights info and tours</a><br>
-<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br>
-Lodging:<br>
+<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br><br>
+Lodging:<br><br>
 <a href="https://www.borealisbasecamp.net/homepage" target="_blank">Igloo experience with northern lights</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 March = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://www.icealaska.org/" target="_blank">Ice Art Around Town</a><br>
 <a href="https://alaskadogmushers.com/open-north-american-championships/" target="_blank">Open North American Championship</a><br>
 <a href="https://www.travelalaska.com/Things-To-Do/Northern-Lights" target="_blank">Northern Lights info and tours</a><br>
-<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br>
-Lodging:<br>
+<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br><br>
+Lodging:<br><br>
 <a href="https://www.borealisbasecamp.net/homepage" target="_blank">Igloo experience with northern lights</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 April = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://runningreindeer.com/" target="_blank">Go on a Reindeer walk</a><br>
 <a href="https://www.travelalaska.com/Things-To-Do/Northern-Lights" target="_blank">Northern Lights info and tours</a><br>
-<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br>
-Lodging:<br>
+<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br><br>
+Lodging:<br><br>
 <a href="https://www.borealisbasecamp.net/homepage" target="_blank">Igloo experience with northern lights</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 May = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://www.northernalaska.com/tours/arctic-circle" target="_blank">Take a day trip to the arctic circle</a><br>
 <a href="https://www.alaska.org/detail/black-spruce-dog-sledding?query=a385a1ad8cef14980c8d30aa020195f5&id=21830" target="_blank">Black Spruce Dog Sledding</a><br>
-<a href="https://georgesonbotanicalgarden.org/" target="_blank">See the botanical gardens</a><br>
-Lodging:<br>
+<a href="https://georgesonbotanicalgarden.org/" target="_blank">See the botanical gardens</a><br><br>
+Lodging:<br><br>
 <a href="https://atasteofalaska.com/" target="_blank">Alaska Lodge</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 June = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://fairbanksalaska.com/midnightsunfestival/" target="_blank">Midnight Sun Festival</a><br>
 <a href="https://www.riverboatdiscovery.com/#tour" target="_blank">Take a river boat tour</a><br>
-<a href="https://georgesonbotanicalgarden.org/" target="_blank">See the botanical gardens</a><br>
-Lodging:<br>
+<a href="https://georgesonbotanicalgarden.org/" target="_blank">See the botanical gardens</a><br><br>
+Lodging:<br><br>
 <a href="https://atasteofalaska.com/" target="_blank">Alaska Lodge</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 July = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://www.fairbankschamber.org/golden-days" target="_blank">Golden Days parade and street fair</a><br>
-<a href="https://georgesonbotanicalgarden.org/" target="_blank">See the botanical gardens</a><br>
-Lodging:<br>
+<a href="https://georgesonbotanicalgarden.org/" target="_blank">See the botanical gardens</a><br><br>
+Lodging:<br><br>
 <a href="https://atasteofalaska.com/" target="_blank">Alaska Lodge</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 August = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://www.tvsfa.org/" target="_blank">Tanana Valley State Fair</a><br>
-<a href="https://georgesonbotanicalgarden.org/" target="_blank">See the botanical gardens</a><br>
-Lodging:<br>
+<a href="https://georgesonbotanicalgarden.org/" target="_blank">See the botanical gardens</a><br><br>
+Lodging:<br><br>
 <a href="https://atasteofalaska.com/" target="_blank">Alaska Lodge</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 September = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://www.alaska.org/destination/fairbanks/parks-and-trails" target="_blank">Go hiking</a><br>
-<a href="https://georgesonbotanicalgarden.org/" target="_blank">See the botanical gardens</a><br>
-Lodging:<br>
+<a href="https://georgesonbotanicalgarden.org/" target="_blank">See the botanical gardens</a><br><br>
+Lodging:<br><br>
 <a href="https://atasteofalaska.com/" target="_blank">Alaska Lodge</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 October = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://www.golddredge8.com/#tour" target="_blank">Pan for Gold</a><br>
 <a href="https://www.travelalaska.com/Things-To-Do/Northern-Lights" target="_blank">Northern Lights info and tours</a><br>
-<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br>
-Lodging:<br>
+<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br><br>
+Lodging:<br><br>
 <a href="https://www.borealisbasecamp.net/homepage" target="_blank">Igloo experience with northern lights</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 November = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://rodsalaskanguideservice.com" target="_blank">Ice fishing</a><br>
 <a href="https://www.travelalaska.com/Things-To-Do/Northern-Lights" target="_blank">Northern Lights info and tours</a><br>
-<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br>
-Lodging:<br>
+<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br><br>
+Lodging:<br><br>
 <a href="https://www.borealisbasecamp.net/homepage" target="_blank">Igloo experience with northern lights</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 December = """
-Things to do:<br>
+Things to do:<br><br>
 <a href="https://rodsalaskanguideservice.com" target="_blank">Snowmobiling</a><br>
 <a href="https://fairbanksalaska.com/solsticefireworks/" target="_blank">Winter solstice festival</a><br>
 <a href="https://www.travelalaska.com/Things-To-Do/Northern-Lights" target="_blank">Northern Lights info and tours</a><br>
-<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br>
-Lodging:<br>
+<a href="https://chenahotsprings.com/" target="_blank">Chena Hot Springs</a><br><br>
+Lodging:<br><br>
 <a href="https://www.borealisbasecamp.net/homepage" target="_blank">Igloo experience with northern lights</a><br>
-<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br>
+<a href="https://chenahotsprings.com/cabins/" target="_blank">Stay in a cabin at the hot springs</a><br><br>
 """
 
 CONTENT = dict()
